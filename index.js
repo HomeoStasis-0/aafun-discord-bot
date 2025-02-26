e980f118805500e12b2d9ffecaedb081ab011bab
HomeoStasis69 <javibetancourt@tamu.edu>
2025-02-26 00:26:15 -0600

    chat

--- index.js
+++ index.js
@@ -19,9 +19,37 @@ function createClient() {
     ]
   });
 
-  client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'chat') {
+      const userMessage = interaction.options.getString('message');
+      await interaction.deferReply();
+
+      try {
+        const response = await groq.chat.completions.create({
+          model: "llama-3.3-70b-versatile",
+          messages: [{ role: "user", content: userMessage }],
+        });
+
+        const reply = response.choices[0]?.message?.content || "Sorry, I couldn't process that request.";
+        await interaction.editReply(reply);
+      } catch (error) {
+        console.error('Error fetching AI response:', error.response?.data || error.message);
+        await interaction.editReply("Sorry, I couldn't process that request.");
+      }
+    }
   });
+  
+function createClient() {
+  client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  });
+
+  client.on('ready', () => {
+    console.log(`We have logged in as ${client.user.tag}`);
+  });
+
 
   client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
@@ -42,44 +70,150 @@ function createClient() {
     }
   });
 
-  client.on('messageCreate', async message => {
-    if (message.author.bot) return;
+  client.on('messageCreate', message => {
+    console.log(`Received message: ${message.content}`);  // Log received messages
 
-    const content_lower = message.content.toLowerCase();
-    console.log(`Received message: ${content_lower}`);
-
-    const regexResponses = [
-      { regex: new RE2('\berm+\b'), url: 'https://tenor.com/view/omori-erm-uuuh-uhh-huh-gif-15238876008948972055' },
-      { regex: new RE2('\bguh+\b'), url: 'https://tenor.com/view/guh-gif-25116077' },
-      { regex: new RE2('\bglorpshit\b'), url: 'https://tenor.com/view/glorp-glorpshit-mad-gif-12826934952903770254' },
-      { regex: new RE2('\bmeow\b'), url: 'https://tenor.com/view/big-poo-big-poo-cat-big-poo-cat-gif-8095478642247689280' },
-      { regex: new RE2('\bmeow\b'), url: 'https://tenor.com/view/larry-larry-cat-chat-larry-meme-chat-meme-cat-gif-10061556685042597078', condition: user => user === "lyxchee" },
-      { regex: new RE2('\bfemboy\b'), url: 'https://tenor.com/view/anime-gif-1742373052751281532', condition: user => user === "homeo_stasis" }
-    ];
-
-    for (let { regex, url, condition } of regexResponses) {
-      if (regex.test(content_lower) && (!condition || condition(message.author.username))) {
-        bot_active = true;
-        console.log('Matched a keyword, sending response');
-        try {
-          const msg = await message.channel.send(url);
-          setTimeout(async () => {
-            try {
-              await msg.delete();
-              console.log('Response deleted');
-            } catch (error) {
-              console.error('Error deleting response:', error);
-            }
-          }, 5000);
-        } catch (error) {
-          console.error('Error sending response:', error);
-        }
-        return;
-      }
+    if (message.author.id === client.user.id) {
+      console.log('Ignoring message from the bot itself');
+      return;
     }
 
-    bot_active = false;
-    console.log('No keyword matched');
+    const content_lower = message.content.toLowerCase();
+    console.log(`Message content in lowercase: ${content_lower}`);
+
+    // const fatRegex = new RE2('\\bfat\\b');
+    // const fatassRegex = new RE2('\\bfatass\\b');
+    // const fattyRegex = new RE2('\\bfatty\\b');
+    // const yourMomRegex = new RE2('\\byour mom\\b');
+    // const urMomRegex = new RE2('\\bur mom\\b');
+    const ermRegex = new RE2('\\berm+\\b');
+    const guhRegex = new RE2('\\bguh+\\b');
+    // const cumRegex = new RE2('\\b(cum|cums|cumming|bust|busting|busted|cream|creams|creaming|creamed)\\b', 'i');
+    const glorpshitRegex = new RE2('\\bglorpshit\\b');
+    const meowRegex = new RE2('\\bmeow\\b');
+    const femboyRegex = new RE2('\\bfemboy\\b');
+
+
+    console.log(`Testing regex patterns against message content...`);
+    // if (urMomRegex.test(content_lower) || yourMomRegex.test(content_lower) || fatRegex.test(content_lower) || fatassRegex.test(content_lower) || fattyRegex.test(content_lower)) {
+    //   bot_active = true;
+    //   console.log('Matched a keyword, sending response');
+    //   message.channel.send('https://tenor.com/view/berserk-skeleton-damn-bro-you-gif-25852196')
+    //     .then(() => console.log('Response sent successfully'))
+    //     .catch(error => console.error('Error sending response:', error));
+    // }
+    if (ermRegex.test(content_lower)) {
+      bot_active = true;
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/omori-erm-uuuh-uhh-huh-gif-15238876008948972055')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+    else if (guhRegex.test(content_lower)) {
+      bot_active = true;
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/guh-gif-25116077')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+    // else if (cumRegex.test(content_lower)) {
+    //   bot_active = true;
+    //   console.log('Matched a keyword, sending a random response');
+    
+    //   // Array of GIF URLs
+    //   const cumGifs = [
+    //     'https://tenor.com/view/anime-magic-senpai-magic-sempai-tejina-senpai-jizz-hands-gif-15061965',
+    //     'https://tenor.com/view/todo-clap-orgasm-daddy-yes-boogie-woogie-gif-21117228',
+    //     'https://tenor.com/view/nut-orgasm-catgirl-anime-nyanners-gif-24080350',
+    //     'https://tenor.com/view/hehehe-creepy-lovely-excited-orgasm-gif-15541428',
+    //     'https://tenor.com/view/anime-watamote-orgasm-twitch-gif-23286923',
+    //     'https://tenor.com/view/catgirl-nekomimi-sexy-anime-gif-20620042',
+    //     'https://tenor.com/view/kiss-anime-gif-25489406'
+    //   ];
+    
+    //   const randomGif = cumGifs[Math.floor(Math.random() * cumGifs.length)];
+    
+    //   message.channel.send(randomGif)
+    //     .then(() => console.log('Response sent successfully'))
+    //     .catch(error => console.error('Error sending response:', error));
+    // }
+    
+    else if (glorpshitRegex.test(content_lower)) {
+      bot_active = true
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/glorp-glorpshit-mad-gif-12826934952903770254')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+    else if (meowRegex.test(content_lower) && message.author.username == "lyxchee") {
+      bot_active = true
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/larry-larry-cat-chat-larry-meme-chat-meme-cat-gif-10061556685042597078')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+    else if (meowRegex.test(content_lower)) {
+      bot_active = true
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/big-poo-big-poo-cat-big-poo-cat-gif-8095478642247689280')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+    else if (femboyRegex.test(content_lower) && message.author.username == "homeo_stasis") {
+      bot_active = true
+      console.log('Matched a keyword, sending response');
+      message.channel.send('https://tenor.com/view/anime-gif-1742373052751281532')
+      .then(msg => {
+        console.log('Response sent successfully');
+        setTimeout(() => {
+          msg.delete()
+            .then(() => console.log('Response deleted'))
+            .catch(error => console.error('Error deleting response:', error));
+        }, 5000);
+      })
+    }
+
+    else {
+      bot_active = false;
+      console.log('No keyword matched');
+    }
+
+    if (bot_active) {
+      // Add any additional functionality here when the bot is active
+      console.log('Bot is active');
+    } else {
+      console.log('Bot is inactive');
+    }
   });
 }
 
